2dfaff6fa69506d3d0c4e62aec29563c5d0f1ee5
Ponnu Sudesh <121781241+ponnu9841@users.noreply.github.com>
2026-02-08 18:28:50 +0530

    Add new allowed origin for CORS

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ app.use(
          const allowedOrigins = [
             "http://localhost:3000",
             "https://mariandesignstudio.in",
+            "https://www.mariandesignstudio.in",
             "https://api.mariandesignstudio.in",
          ];
          if (!origin || allowedOrigins.indexOf(origin) !== -1) {
